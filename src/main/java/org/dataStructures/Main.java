2a8c59b29e9935779c95bab5beb0e6d42df73078
Akhil Ramagiri <akhilramagiri11@gmail.com>
2024-02-29 18:21:40 +1100

    add array's

--- src/main/java/org/dataStructures/Main.java
+++ src/main/java/org/dataStructures/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.dataStructures;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
         System.out.printf("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
